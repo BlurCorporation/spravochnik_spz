9121673d001f54b3ca78e77f18c636995a236d0a
Maksim <mayden1127@yandex.ru>
2023-01-19 22:22:30 +0300

    Initial Commit

---
+++ spravochnik_spz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spravochnik_spz
+//
+//  Created by Максим Косников on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
