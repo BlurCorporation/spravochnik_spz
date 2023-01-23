9302233b41d9d52319e8ebcacf7b03241e3fa918
Maksim <mayden1127@yandex.ru>
2023-01-23 20:43:37 +0300

    Внесение изменение по codestyle и папочка CELLS в CORE

--- spravochnik_spz/Application/SceneDelegate.swift
+++ spravochnik_spz/Application/SceneDelegate.swift
@@ -11,11 +11,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(
-        _ scene: UIScene,
-        willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions
-    ) {
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
         setupRootViewController(windowScene: windowScene)
@@ -27,7 +25,7 @@ private extension SceneDelegate {
     func setupRootViewController(windowScene: UIWindowScene) {
         let window = UIWindow(windowScene: windowScene)
         let sceneBuildManager: Buildable = SceneBuildManager()
-        let viewController = sceneBuildManager.buildSplashScreen()
+        let viewController = sceneBuildManager<#.Screen()#> // Подставить свой view controller
         let navigationController = UINavigationController(rootViewController: viewController)
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
