c3795a3046b7ddec09eb99302bf9d30c7fa97c7b
Maksim <mayden1127@yandex.ru>
2023-06-24 12:18:49 +0300

    Fix hidden savedCalcScreen tabbar after navigation from calcs

--- spravochnik_spz/Scenes/SavedCalculations/SavedCalculationsViewController.swift
+++ spravochnik_spz/Scenes/SavedCalculations/SavedCalculationsViewController.swift
@@ -56,6 +56,7 @@ final class SavedCalculationsViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.isNavigationBarHidden = true
+        tabBarController?.tabBar.isHidden = false
     }
 }
 
