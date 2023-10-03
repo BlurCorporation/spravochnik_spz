5de0ac12be336337129069040648a2d6a09d2b0d
Maksim <mayden1127@yandex.ru>
2023-10-03 20:33:08 +0300

    da

--- spravochnik_spz/Core/Services/PDFService.swift
+++ spravochnik_spz/Core/Services/PDFService.swift
@@ -18,35 +18,38 @@ final class PDFService {
 
 extension PDFService: PDFServiceProtocol {
     func generatePDF(from text: String) -> URL? {
-        // 1
-        let pdfMetaData = [
-            kCGPDFContextCreator: "Flyer Builder",
-            kCGPDFContextAuthor: "raywenderlich.com"
-        ]
-        let format = UIGraphicsPDFRendererFormat()
-        format.documentInfo = pdfMetaData as [String: Any]
+//        // 1
+//        let pdfMetaData = [
+//            kCGPDFContextCreator: "Flyer Builder",
+//            kCGPDFContextAuthor: "raywenderlich.com"
+//        ]
+//        let format = UIGraphicsPDFRendererFormat()
+//        format.documentInfo = pdfMetaData as [String: Any]
+//
+//        // 2
+//        let pageWidth = 8.5 * 72.0
+//        let pageHeight = 11 * 72.0
+//        let pageRect = CGRect(x: 0, y: 0, width: pageWidth, height: pageHeight)
+//
+//        // 3
+//        let renderer = UIGraphicsPDFRenderer(bounds: pageRect, format: format)
+//        // 4
+//        let data = renderer.pdfData { (context) in
+//            // 5
+//            context.beginPage()
+//            // 6
+//            let attributes = [
+//                NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 72)
+//            ]
+//            let text = "I'm a PDF!"
+//            text.draw(at: CGPoint(x: 0, y: 0), withAttributes: attributes)
+//
+//        }
         
-        // 2
-        let pageWidth = 8.5 * 72.0
-        let pageHeight = 11 * 72.0
-        let pageRect = CGRect(x: 0, y: 0, width: pageWidth, height: pageHeight)
+        let url = Bundle.main.path(forResource: "securityAlarm1", ofType: "pdf")
+        let data = NSData(contentsOfFile: url!)
+        let pdfDocument = PDFDocument(data: data! as Data)
         
-        // 3
-        let renderer = UIGraphicsPDFRenderer(bounds: pageRect, format: format)
-        // 4
-        let data = renderer.pdfData { (context) in
-            // 5
-            context.beginPage()
-            // 6
-            let attributes = [
-                NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 72)
-            ]
-            let text = "I'm a PDF!"
-            text.draw(at: CGPoint(x: 0, y: 0), withAttributes: attributes)
-            
-        }
-        
-        let pdfDocument = PDFDocument(data: data)
         guard let pdfDocument = pdfDocument else { return nil }
         do {
             let tempDirectory = FileManager.default.temporaryDirectory
